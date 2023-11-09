e7ec23ff8a5fe807bf9e635c29e23d20e6f25080
Vlad <vlad.bliznyuk.00mail.ru>
2023-11-09 23:29:55 +0300

    2  commit

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,6 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+if (number1 % 2){
+    console.log(`${number1} :Это нечётное число`)
+} else{
+    console.log(`${number1} :Это чётное число`)
+};
